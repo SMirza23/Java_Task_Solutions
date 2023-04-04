25296bebef3385c219763939cde4da49b6e23097
SMir <shokhidamir@gmail.com>
2023-04-04 14:46:02 +0200

    Practice

---
+++ src/Practice/GradeLevel_If_Statements.java
@@ -0,0 +1,28 @@
+package Practice;
+
+public class GradeLevel_If_Statements {
+
+    public static void main(String[] args) {
+
+        int gradeLevel = 6;
+        String result = ""; //temporary value
+
+        if( gradeLevel >= 1 && gradeLevel <= 5){
+            result = "Elementary School";
+        }
+        if(gradeLevel >= 6 && gradeLevel <= 8){
+            result = "Middle School";
+        }
+        if(gradeLevel >= 9 && gradeLevel <= 12){
+            result = "High School";
+        }
+        if(gradeLevel >= 13 && gradeLevel <= 16){
+            result = "College";
+        }
+        if(gradeLevel>= 17 && gradeLevel<= 18){
+            System.out.println("Grad School");
+        }
+
+        System.out.println(result);
+    }
+}
